e003c8e93e148f8f572b5b667b37c6f3dc8994b9
Belu <belulop_@MacBook-Pro-de-Belu.local>
2022-07-29 17:30:21 -0300

    Bot

--- sprint_05/routes/userRoutes.js
+++ sprint_05/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
 //requerir el controlador
 const userController = require('../controllers/userController');
 
