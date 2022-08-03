53027f89e23ed421b1c54cf8847687aff077bb5d
Belu <belulop_@MacBook-Pro-de-Belu.local>
2022-08-03 18:03:41 -0300

    Confirm password validator

--- sprint_05/routes/userRoutes.js
+++ sprint_05/routes/userRoutes.js
@@ -33,7 +33,14 @@ const registerValidations = [
    body('usuario').notEmpty().withMessage('El campo usuario no puede quedar vacío'),
    body('email').isEmail().withMessage('Debes ingresar un email válido'),
    body('contrasena').isLength({ min: 8}).withMessage('La contraseña debe contener minimo 8 caracteres'),
-   body('contrasenaConf').isLength({ min: 8}).withMessage('La contraseña debe contener minimo 8 caracteres'),
+   body('contrasenaConf').isLength({ min: 8}).withMessage('La contraseña debe contener minimo 8 caracteres').bail().custom((value, { req }) => {
+      if (value !== req.body.contrasena) {
+        throw new Error('Las contraseñas deben ser iguales');
+      }else{
+         return true;
+      }
+      
+    }),
    body('fechaNacimiento').isDate().withMessage('Debes ingresar tu fecha de nacimiento'),
    body('avatar').custom((value, { req } ) => {
       let file = req.file;
