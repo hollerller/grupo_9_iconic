7cc0b9b27ea9be1e1db237efcac7ff77e1321527
Belu <belulop_@MBP-de-Belu.fibertel.com.ar>
2022-08-05 12:02:43 -0300

    middlewares

--- sprint_05/middlewares/authMiddleware.js
+++ sprint_05/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const authMiddleware = (req,res,next)=>{
     if(!req.session.userLogged){
-        return res.redirect('/users/login');
+        return res.redirect('/users/çlogin');
     }
     next()
 }
