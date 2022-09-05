c7ae9bb4c1de2947c53930703ef162ea12d73d31
hollerller <hollerller@gmail.com>
2022-09-04 23:18:17 -0500

    Registro de usuarios completado con validaciones

--- sprint_06/controllers/userController.js
+++ sprint_06/controllers/userController.js
@@ -18,13 +18,14 @@ const usersController = {
     // Mostrar perfil de usuario
     userID: (req, res) => {
         //console.log(req.session.userLogged);
-        db.User.findByPk(req.session.id,{
+        let userToDisplay = req.session.userLogged;
+        db.User.findByPk(userToDisplay.id,{
             include:[
                 {association: "roles"},
                 {association: "countries"}
             ]   
         }).then(user =>{
-                res.render('userDetail', {user:user})
+                res.render('userDetail', {usuario: user})
             })
     },
     // Mostrar Formulario de registro
@@ -32,9 +33,7 @@ const usersController = {
     register: (req, res) => {
         let role = db.Role.findAll();
         let country = db.Country.findAll();
-   /*     let usuario1 = db.User.findByPk(1).then((result) => {
-            console.log(usuario1);
-        })*/
+
         Promise.all([role, country])
             .then(function([role, country]){
             res.render('register', {
@@ -47,41 +46,49 @@ const usersController = {
     //Procesar formulario de registro
 
     createUser: (req, res) => {
+        const registerValidation = validationResult(req)
         console.log(req.body);
-        db.User.create ({
-            full_name: req.body.nombreYApellido,
-            user_name: req.body.usuario,
-            email: req.body.email,
-            avatar: req.file.filename,
-            password: bcryptjs.hashSync(req.body.contrasena, 10),
-            birthday: req.body.fechaNacimiento,
-            role_id: req.body.role,
-            country_id: req.body.country
-        })
-    res.redirect("login"); 
-        /*const registerValidation = validationResult(req);
-        console.log(req.body);
-        if (registerValidation.errors.length > 0) {
+
+    if (registerValidation.errors.length > 0) {
+        
+        let role = db.Role.findAll();
+        let country = db.Country.findAll();
+
+        Promise.all([role, country])
+            .then(function([role, country]){
             res.render('register', {
                 registerErrors: registerValidation.mapped(),
-                oldData: req.body
+                oldData: req.body,
+                role: role,
+                country: country
             })
-        } else {
-            let userExists = db.User.findOne({
-                where: {
-                    email: req.body.email
-                }
-            })          
+        })
+  } else {
+    db.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(userExists => {
             if (userExists) {
+
+                let role = db.Role.findAll();
+        let country = db.Country.findAll();
+
+        Promise.all([role, country])
+            .then(function([role, country]){
+
                 res.render('register', {
                     registerErrors: {
                         email: {
                             msg: 'Este email ya está registrado'
                         }
                     },
-                    oldData: req.body
+                    oldData: req.body,
+                    role: role,
+                    country: country
                 })
-            } else {
+                })
+            } else { 
                 db.User.create ({
                     full_name: req.body.nombreYApellido,
                     user_name: req.body.usuario,
@@ -92,14 +99,13 @@ const usersController = {
                     role_id: req.body.role,
                     country_id: req.body.country
                 })
-                }
-            res.redirect("login");
-      
-        }
-        */
-    },
+                res.redirect("login");
+            }
 
-
+        })
+       
+    } 
+ },
 
     // Mostrar formulario de login
 
@@ -108,25 +114,25 @@ const usersController = {
         res.render('login');
 
     },
+  
     // Procesar login
 
     processLogin: (req, res) => {
-        let userToLogin = db.User.findOne({
+        db.User.findOne({
             where: {
                 email: req.body.email
             }
-        }).then
-// prueba test
-        if (userToLogin) {
-            let validPassword = bcryptjs.compareSync(req.body.contrasena, userToLogin.password);
-            if (validPassword) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
+        }).then(usuario => {
+            if (usuario) {
+                let validPassword = bcryptjs.compareSync(req.body.contrasena, usuario.password);
 
-                if (req.body.recordarUsuario) {
-                    res.cookie('usernameCookie', req.body.email, { maxAge: 86400000 }); // cookie que dura 24 horas
-                }
-                res.redirect('profile')
+                if (validPassword) {
+                    delete usuario.password;
+                    req.session.userLogged = usuario;
+                    if (req.body.recordarUsuario) {
+                        res.cookie('usernameCookie', req.body.email, { maxAge: 86400000 }); // cookie que dura 24 horas
+                    }
+                    res.redirect('profile')
 
             } else {
                 res.render('login', {
@@ -146,63 +152,57 @@ const usersController = {
                     }
                 }
             })
-        }
+        }})
+// prueba test
+       
     },
+
     editUser: (req, res) => {
-            //pedidos asincrónicos//
-            let user = db.User.findByPk(req.session.id)
-            let roles = db.Role.findAll();
-            let countries = db.Country.findAll();
-            
-          Promise.all([user, roles, countries])
-            .then(([user, role, country])=>{
-                res.render('userEdit',{
-                    user: user,
-                    roles: roles,
-                    countries: countries
+        let userToEdit = req.session.userLogged;
+
+        let role = db.Role.findAll();
+        let country = db.Country.findAll();
+
+          Promise.all([role, country])
+            .then(([role, country])=>{
+             
+              res.render('userEdit',{
+                    oldData: userToEdit,
+                    role: role,
+                    country: country
                 })
             })
     },
 
     processEdition: (req, res) => {
-        const registerValidation = validationResult(req);
-        
-        if (registerValidation.errors.length > 0) {
-            res.render('register', {
-                registerErrors: registerValidation.mapped(),
-                oldData: req.session.userLogged,
-
-            })
-
-        } else {
-
-            let userToEdit = req.session.userLogged;
-
-            db.User.update({
-                full_name: req.body.nombreYApellido,
-                user_name: req.body.usuario,
-                email: req.body.email,
-                avatar: req.file.filename,
-                password: bcryptjs.hashSync(req.body.contrasena, 10),
-                birthday: req.body.fechaNacimiento,
-                role_id: req.body.role,
-                country_id: req.body.country
-            }, {
-            where: {
-                id: req.session.id
-            }
-        })
-            let user = db.User.findOne({
-                where: {
-                    email: req.session.email
-                }
-            });
-            delete user.password;
-            req.session.userLogged = user;
-            res.render('userDetail',{ usuario: user })
+      // const registerValidation = validationResult(req);
+   
+       db.User.update({
+        full_name: req.body.nombreYApellido,
+        user_name: req.body.usuario,
+        email: req.body.email,
+        avatar: req.file.filename,
+        password: bcryptjs.hashSync(req.body.contrasena, 10),
+        birthday: req.body.fechaNacimiento,
+        role_id: req.body.role,
+        country_id: req.body.country
+    }, {
+    where: {
+        id: req.session.id
     }
-        
-    },
+})
+   let user = db.User.findOne({
+        where: {
+            email: req.session.email
+        }
+    });
+    delete user.password;
+    req.session.userLogged = user;
+    res.render('userDetail',{ usuario: user })
+    
+
+},
+          
 
     logout: (req, res) => {
         req.session.destroy();
