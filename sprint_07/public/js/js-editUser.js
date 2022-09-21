fe70b919824ff047cf6dbc78745bb23e39b581e1
Belu <belulop_@MacBook-Pro-de-Belu.local>
2022-09-21 12:38:39 -0300

    validaciones edit user

--- sprint_07/public/js/js-editUser.js
+++ sprint_07/public/js/js-editUser.js
@@ -1,75 +1,121 @@
 window.addEventListener("load",()=>{
+    //Capturamos el formulario//
     let form = document.getElementById("editUser-form");
 
-    
-    form.addEventListener("submit",(e)=>{
-        let errors = [];
-        
-
-        // ulErrors.innerHTML = ""
-
-        let fullName = document.getElementById("nombre-apellido");
-
-        fullName.addEventListener("keydown",()=>{
-           return fullName.value = ""
-        })
-        if(fullName.value == "" || fullName.value == null){
-            errors.push("El campo Nombre y Apellido está vacío");
-        }else if(fullName.value.length < 4){
-            errors.push("Debes usar al menos 4 caracteres")
-        }
-
-        let userName = document.getElementById("usuario");
-        if(userName.value == ""){
-            errors.push("El campo usuario está vacío");
-        }else if(userName.value.length < 4){
-            errors.push("Debes usar al menos 4 caracteres")
-        }else if(userName.value.includes(" ")){
-            errors.push("El campo usuario no puede contener espacios vacíos")
-        }
-
-        let email = document.getElementById("mail");
-        if(email.value == ""){
-            errors.push("El campo email está vacío");
-        }else if(email.value.length < 9){
-            errors.push("Debes usar al menos 4 caracteres")
-        }else if(!email.value.includes('@')){
-            errors.push("Debes colocar una dirección de correo válida")
-        }
-
-
-        let password = document.getElementById("contrasena");
-        if(password.value == ""){
-            errors.push("Debes colocar una contraseña");
-        }else if(password.value.length < 6){
-            errors.push("Debes usar al menos 6 caracteres")
-        }
-
-
-
-        let passwordConfirmation = document.getElementById("contrasenaConfirmada");
-        if(passwordConfirmation.value == ""){
-            errors.push("Debes colocar una contraseña");
-        }else if(passwordConfirmation.value.length < 6){
-            errors.push("Debes usar al menos 6 caracteres")
-        }else if(password != passwordConfirmation){
-            errors.push("Las contraseñas deben ser iguales")
-        }
-        let birthday = document.getElementById("fechaNacimiento");
-        if(birthday.value == ""){
-            errors.push("Debes colocar tu fecha de nacimiento")
-        }
-        //MOSTRAR ERRORES
-        if (errors.length > 0){
-            e.preventDefault();
-    
-            let ulErrors = document.getElementById("errors");
-            for(let i = 0;i < errors.length; i++){
-                ulErrors.innerHTML += "<li>" + errors[i] + "</li>"
-            }
-            
-        }
-        
-    })
    
+  
+            //Capturamos el campo full name y el div de sus errores//
+            let fullName = document.getElementById("nombre-apellido");
+            let fullNameValidations = document.querySelector(".fullname_validations")
+            
+
+            //Validaciones del campo Full name//
+            fullName.addEventListener("input",()=>{
+                if(fullName.value.length < 6){
+                    fullNameValidations.classList.remove("checked")
+                    fullNameValidations.classList.add("alarm")
+                    fullNameValidations.innerHTML = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }else if(fullName.value.length >= 6){
+                    fullNameValidations.classList.remove("alarm")
+                    fullNameValidations.classList.add("checked")
+                    fullNameValidations.innerHTML = "<p>Perfecto!</p>"
+                }
+            })
+            fullName.addEventListener("blur",()=>{
+                if(fullName.value == ""){
+                    fullNameValidations.classList.toggle("checked")
+                    fullNameValidations.classList.add("alarm")
+                    fullNameValidations = "<p>Recordá que este campo no puede estar vacio</p>"
+                }else if(fullName.value <6){
+                    fullNameValidations.classList.toggle("checked")
+                    fullNameValidations.classList.add("alarm")
+                    fullNameValidations = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }
+            })
+            //Capturamos el input user name y su div de validaciones//
+            let userName = document.getElementById("usuario");
+            let userNameValidations = document.querySelector("div.username_validations");
+            //Validaciones del campo user name//
+            userName.addEventListener("input",()=>{
+                if(userName.value.length < 6){
+                    userNameValidations.classList.remove("checked")
+                    userNameValidations.classList.add("alarm")
+                    userNameValidations.innerHTML = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }else if(userName.value.length >= 6){
+                    userNameValidations.classList.remove("alarm")
+                    userNameValidations.classList.add("checked")
+                    userNameValidations.innerHTML = "<p>Perfecto!</p>"
+                }
+            })
+            userName.addEventListener("blur",()=>{
+                if(userName.value == ""){
+                    userNameValidations.classList.toggle("checked")
+                    userNameValidations.classList.add("alarm")
+                    userNameValidations = "<p>Recordá que este campo no puede estar vacio</p>"
+                }else if(userName.value <6){
+                    userNameValidations.classList.toggle("checked")
+                    userNameValidations.classList.add("alarm")
+                    userNameValidations = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }
+            })
+
+            //capturamos el campo mail y su div de validaciones//
+            let email = document.getElementById("mail");
+            let emailValidations = document.querySelector("div.email_validations");
+
+            // validaciones campo email//
+            email.addEventListener("input",()=>{
+                if(email.value.length < 6){
+                    emailValidations.classList.remove("checked")
+                    emailValidations.classList.add("alarm")
+                    emailValidations.innerHTML = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }else if(email.value.length >= 6 && email.value.includes('@')){
+                    emailValidations.classList.remove("alarm")
+                    emailValidations.classList.add("checked")
+                    emailValidations.innerHTML = "<p>Perfecto!</p>"
+                }else if(email.value.length<6 || !email.value.includes("@")){
+                    emailValidations.classList.remove("checked")
+                    emailValidations.classList.add("alarm")
+                    emailValidations.innerHTML = "<p>Ingresá un email válido</p>"
+                }
+            })
+            email.addEventListener("blur",()=>{
+                if(email.value == ""){
+                    emailValidations.classList.toggle("checked")
+                    emailValidations.classList.add("alarm")
+                    emailValidations = "<p>Recordá que este campo no puede estar vacio</p>"
+                }else if(email.value <6){
+                    emailValidations.classList.toggle("checked")
+                    emailValidations.classList.add("alarm")
+                    emailValidations = "<p>Recordá usar un mínimo de 6 caracteres</p>"
+                }else if(!email.value.includes("@")){
+                    emailValidations.classList.remove("checked")
+                    emailValidations.classList.add("alarm")
+                    emailValidations.innerHTML = "<p>Ingresá un email válido</p>"
+                }
+            })
+
+            //capturamos el input de fecha de nacimiento y su div de validaciones//
+            let birthday = document.getElementById("fechaNacimiento");
+            let birthdayValidations = document.querySelector("div.birthday_validations");
+
+            birthday.addEventListener("input",()=>{
+                if(birthday.value == ""){
+                    birthdayValidations.classList.remove("checked")
+                    birthdayValidations.classList.add("alarm")
+                    birthdayValidations.innerHTML = "<p>Por favor, colocá tu fecha de nacimiento</p>"
+                }
+            })
+            birthday.addEventListener("blur",()=>{
+                if(birthday.value == ""){
+                    birthdayValidations.classList.remove("checked")
+                    birthdayValidations.classList.add("alarm")
+                    birthdayValidations.innerHTML = "<p>Por favor, colocá tu fecha de nacimiento</p>"
+                }else{
+                    birthdayValidations.classList.remove("alarm")
+                    birthdayValidations.classList.add("checked")
+                    birthdayValidations.innerHTML = "<p>Genial!</p>"
+                }
+            })
+       
 })
